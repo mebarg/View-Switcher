3406d898f794a641b8bb69d6c75c2b8f87c41054
Mario Berazategui <mebarg@Mac-Pro-de-Mario.local>
2013-08-11 10:06:42 -0300

    Initial Commit

---
+++ View Switcher/MEBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MEBAppDelegate.h
+//  View Switcher
+//
+//  Created by Mario Berazategui on 11/08/13.
+//  Copyright (c) 2013 Mario Berazategui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ View Switcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  View Switcher
+//
+//  Created by Mario Berazategui on 11/08/13.
+//  Copyright (c) 2013 Mario Berazategui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MEBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MEBAppDelegate class]));
+    }
+}
